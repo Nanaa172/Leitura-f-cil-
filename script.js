595a5355a9792e69de1ae0a1b935cf87214bcf41
Jordana <araujo.jordana@escola.pr.gov.br>
2026-06-22 15:35:21 +0000

    Altercação 1

---
+++ script.js
@@ -0,0 +1,17 @@
+let tamanhoFonte = 20;
+
+function aumentarFonte(){
+    tamanhoFonte += 2;
+    document.getElementById("texto").style.fontsize = tamanhoFonte + "px";
+}
+
+function diminuirFonte(){
+    if (tamanhoFonte > 14){
+        tamnhoFonte -= 2;
+        document.getElementById("texto").style.fontsize = tamanhoFonte + "px";
+    }
+}
+
+function ativarContraste(){
+    document.body.classList.toggle("contraste");
+}
